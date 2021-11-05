741ff13c28e29b293643fa7faf4e0305e25b3a75
daniel <danugarri@gmail.com>
2021-11-05 20:05:50 +0100

    adding redirection logic and so improving the user experience

--- src/components/Initial/Initial.js
+++ src/components/Initial/Initial.js
@@ -3,11 +3,13 @@ import './Initial.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 import { faIdCard } from '@fortawesome/free-solid-svg-icons';
-import { NavLink } from "react-router-dom";
+import { NavLink, Redirect} from "react-router-dom";
 import swal from 'sweetalert';
+import { useState } from 'react';
 
 export const Initial= ({dni,setDni}) => {
-
+    // handle redirection to the solution
+    const [redirection, setRedirection] = useState(false)
     
     //icons
      const card= <FontAwesomeIcon icon={faIdCard} />
@@ -17,13 +19,29 @@ export const Initial= ({dni,setDni}) => {
      const onHandleSubmit= (event) =>{
          event.preventDefault();
           let dniNumber= event.target.querySelector('input[name="number"]').value;
-          dniNumber !=='' && dniNumber.length===8 ? setDni(() => dniNumber) : swal('No has introducido un DNI Válido')
+          if(dniNumber !=='' && dniNumber.length===8){
+              setRedirection(true)
+              setDni(() => dniNumber) 
+          } 
+          else{
+             swal('No has introducido un DNI Válido')
+          } 
          //Reset function
          const reset = () => {
              dniNumber= event.target.querySelector('input[name="number"]').value='';
          }
          reset()
         }
+        //redirection with dni set
+        
+           if(redirection === true) {
+           return <Redirect to ="/transform" ></Redirect>
+           }  
+           else{
+               console.log(" el dni no ha cambiado")
+           }
+     //manage redirection
+     const handleClick = () => setRedirection(false)
 
     return (
         <main>
@@ -35,9 +53,9 @@ export const Initial= ({dni,setDni}) => {
                 </div>
                 <input  type='number' htmlFor='number' name='number' placeholder='EJ: 12644553'/>
                 {/*nav to transform and trigger event*/}
-                <input type='submit' value='Calcular letra'/>
-                <button><NavLink to = './transform' className='buttonLink'> Ver</NavLink></button>
-                <button><NavLink to = './' className='buttonLink'>Ir a inicio</NavLink></button>
+                <input type='submit' value='Calcular letra' />
+                {/*<button><NavLink to = './transform' className='buttonLink' onClick = {handleClick}> Ver</NavLink></button>*/}
+                <button><NavLink to = './' className='buttonLink' onClick = {handleClick}>Ir a inicio</NavLink></button>
             </form>
         </main>
     )
